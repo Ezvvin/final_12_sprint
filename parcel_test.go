622816f26f43e987e540381650d2cca157e4eb9a
Ezvvin <pavel.klimov2012@ya.ru>
2024-06-03 12:06:02 +0300

    fix_db_for_tests

--- parcel_test.go
+++ parcel_test.go
@@ -6,13 +6,14 @@ import (
 	"testing"
 	"time"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 var (
-	// randSource — это источник псевдослучайных чисел.
+	// randSource источник псевдо случайных чисел.
 	// Для повышения уникальности в качестве seed
-	// используется текущее время в unix-формате в виде числа
+	// используется текущее время в unix формате (в виде числа)
 	randSource = rand.NewSource(time.Now().UnixNano())
 	// randRange использует randSource для генерации случайных чисел
 	randRange = rand.New(randSource)
@@ -30,101 +31,82 @@ func getTestParcel() Parcel {
 
 // TestAddGetDelete проверяет добавление, получение и удаление посылки
 func TestAddGetDelete(t *testing.T) {
-	// prepare
+
 	db, err := sql.Open("sqlite", "tracker.db")
-	if err != nil {
-		require.NoError(t, err)
-	}
+	require.NoError(t, err)
 	defer db.Close()
+
 	store := NewParcelStore(db)
 	parcel := getTestParcel()
-
 	// add
-	parcel.Number, err = store.Add(parcel)
-
+	p, err := store.Add(parcel)
+	parcel.Number = p
 	require.NoError(t, err)
-	require.NotEmpty(t, parcel.Number)
-
+	require.NotEmpty(t, p)
 	// get
-	stored, err := store.Get(parcel.Number)
-
+	gp, err := store.Get(p)
 	require.NoError(t, err)
-	require.Equal(t, parcel, stored)
+	require.Equal(t, parcel, gp)
 
 	// delete
-	err = store.Delete(parcel.Number)
+	err = store.Delete(p)
+	require.NoError(t, err)
 
-	stored, err = store.Get(parcel.Number)
-	require.Equal(t, sql.ErrNoRows, err)
+	_, err = store.Get(p)
+	require.ErrorIs(t, err, sql.ErrNoRows)
 }
 
 // TestSetAddress проверяет обновление адреса
 func TestSetAddress(t *testing.T) {
-	// prepare
+
 	db, err := sql.Open("sqlite", "tracker.db")
-	if err != nil {
-		require.NoError(t, err)
-	}
+	require.NoError(t, err)
 	defer db.Close()
+
 	store := NewParcelStore(db)
 	parcel := getTestParcel()
 
 	// add
-	parcel.Number, err = store.Add(parcel)
-
+	p, err := store.Add(parcel)
 	require.NoError(t, err)
-	require.NotEmpty(t, parcel.Number)
+	require.NotEmpty(t, p)
 
-	// set address
 	newAddress := "new test address"
-	err = store.SetAddress(parcel.Number, newAddress)
-
+	err = store.SetAddress(p, newAddress)
 	require.NoError(t, err)
 
-	// check
-	stored, err := store.Get(parcel.Number)
-
+	// get
+	gp, err := store.Get(p)
 	require.NoError(t, err)
-	require.Equal(t, newAddress, stored.Address)
+	require.Equal(t, newAddress, gp.Address)
 }
 
 // TestSetStatus проверяет обновление статуса
 func TestSetStatus(t *testing.T) {
-	// prepare
 	db, err := sql.Open("sqlite", "tracker.db")
-	if err != nil {
-		require.NoError(t, err)
-	}
+	require.NoError(t, err)
 	defer db.Close()
+
 	store := NewParcelStore(db)
 	parcel := getTestParcel()
-
-	// add
-	parcel.Number, err = store.Add(parcel)
-
+	//add
+	p, err := store.Add(parcel)
 	require.NoError(t, err)
-	require.NotEmpty(t, parcel.Number)
+	require.NotEmpty(t, p)
 
-	// set status
-	err = store.SetStatus(parcel.Number, ParcelStatusSent)
-
+	err = store.SetStatus(p, ParcelStatusSent)
 	require.NoError(t, err)
-
-	// check
-	stored, err := store.Get(parcel.Number)
-
+	//get
+	gp, err := store.Get(p)
 	require.NoError(t, err)
-	require.Equal(t, ParcelStatusSent, stored.Status)
+	require.Equal(t, ParcelStatusSent, gp.Status)
 }
 
-// TestGetByClient проверяет получение посылок по идентификатору клиента
 func TestGetByClient(t *testing.T) {
-	// prepare
 	db, err := sql.Open("sqlite", "tracker.db")
-	if err != nil {
-		require.NoError(t, err)
-	}
+	require.NoError(t, err)
 	defer db.Close()
+
 	store := NewParcelStore(db)
 
 	parcels := []Parcel{
@@ -134,34 +116,27 @@ func TestGetByClient(t *testing.T) {
 	}
 	parcelMap := map[int]Parcel{}
 
-	// задаём всем посылкам одного клиента
 	client := randRange.Intn(10_000_000)
 	parcels[0].Client = client
 	parcels[1].Client = client
 	parcels[2].Client = client
 
-	// add
 	for i := 0; i < len(parcels); i++ {
 		id, err := store.Add(parcels[i])
-
 		require.NoError(t, err)
 		require.NotEmpty(t, id)
-
 		parcels[i].Number = id
 		parcelMap[id] = parcels[i]
 	}
 
-	// get by client
-	storedParcels, err := store.GetByClient(client)
-
+	var storedParcels []Parcel
+	storedParcels, err = store.GetByClient(client)
 	require.NoError(t, err)
-	require.Len(t, storedParcels, len(parcels))
+	assert.Equal(t, len(parcels), len(storedParcels))
 
-	// check
 	for _, parcel := range storedParcels {
-		expectedParcel, ok := parcelMap[parcel.Number]
-
+		_, ok := parcelMap[parcel.Number]
 		require.True(t, ok)
-		require.Equal(t, expectedParcel, parcel)
+		assert.Equal(t, parcelMap[parcel.Number], parcel)
 	}
 }
